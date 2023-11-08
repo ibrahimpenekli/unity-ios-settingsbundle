ecff4e0a409e51c177e60b68efe1185aaffe804c
H. Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-11-08 18:34:37 +0300

    Get/set preferences in Unity scene

---
+++ Assets/Scripts/BoolPreferenceSetter.cs
@@ -0,0 +1,15 @@
+﻿namespace Inscept.iOS.Settings.Examples
+{
+    public class BoolPreferenceSetter : PreferenceSetter<bool>
+    {
+        protected override bool GetValue(string key)
+        {
+            return PlayerPrefsExt.GetBool(key);
+        }
+
+        protected override void SetValue(string key, bool value)
+        {
+            PlayerPrefsExt.SetBool(key, value);
+        }
+    }
+}
---
+++ Assets/Scripts/PreferenceSetter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Inscept.iOS.Settings.Examples
+{
+    public abstract class PreferenceSetter<T> : MonoBehaviour
+    {
+        public string identifier;
+        public UnityEvent<T> setter;
+
+        private void Start()
+        {
+            setter.Invoke(GetValue(identifier));
+        }
+
+        public void SetValue(T value)
+        {
+            SetValue(identifier, value);
+            PlayerPrefs.Save();
+        }
+
+        protected abstract T GetValue(string key);
+        protected abstract void SetValue(string key, T value);
+    }
+}
